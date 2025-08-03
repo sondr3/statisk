df8bb50c4f8ade773946f3d01e846d618d7c2295
Sondre Aasemoen <sondre@eons.io>
2025-08-03 19:25:21 +0200

    Update compression to use StatiskIgnore

--- src/compress.rs
+++ src/compress.rs
@@ -1,11 +1,13 @@
-use std::{io::prelude::*, path::Path};
+use std::{
+    io::prelude::*,
+    path::{Path, PathBuf},
+};
 
 use anyhow::{Context, Result};
 use brotli::{CompressorWriter, enc::BrotliEncoderParams};
 use flate2::{Compression, write::GzEncoder};
-use walkdir::DirEntry;
 
-use crate::utils::{append_extension, find_files};
+use crate::{ignorer::StatiskIgnore, utils::append_extension};
 
 const VALID_EXTENSIONS: [&str; 15] = [
     "html", "css", "js", "xml", "css", "cjs", "mjs", "json", "txt", "svg", "map", "ttf", "otf",
@@ -13,29 +15,31 @@ const VALID_EXTENSIONS: [&str; 15] = [
 ];
 
 pub fn folder(folder: &Path) -> Result<()> {
-    find_files(folder, compressible_files).try_for_each(|f| {
-        let content = std::fs::read(&f)?;
-        let gzip = append_extension(&f, "gz");
+    StatiskIgnore::walk_dir(folder)
+        .filter(compressible_files)
+        .try_for_each(|f| {
+            let content = std::fs::read(&f)?;
+            let gzip = append_extension(&f, "gz");
 
-        let mut gzip_encoder = GzEncoder::new(Vec::new(), Compression::best());
-        gzip_encoder.write_all(&content)?;
-        let gzipped = gzip_encoder.finish()?;
+            let mut gzip_encoder = GzEncoder::new(Vec::new(), Compression::best());
+            gzip_encoder.write_all(&content)?;
+            let gzipped = gzip_encoder.finish()?;
 
-        let brotli = append_extension(&f, "br");
-        let brotli_params = BrotliEncoderParams::default();
-        let mut brotli_encoder = CompressorWriter::with_params(Vec::new(), 4096, &brotli_params);
-        brotli_encoder.write_all(&content)?;
+            let brotli = append_extension(&f, "br");
+            let brotli_params = BrotliEncoderParams::default();
+            let mut brotli_encoder =
+                CompressorWriter::with_params(Vec::new(), 4096, &brotli_params);
+            brotli_encoder.write_all(&content)?;
 
-        std::fs::write(gzip, gzipped).context("Failed to write compressed file")?;
-        std::fs::write(brotli, brotli_encoder.into_inner())
-            .context("Failed to write compressed file")
-    })
+            std::fs::write(gzip, gzipped).context("Failed to write compressed file")?;
+            std::fs::write(brotli, brotli_encoder.into_inner())
+                .context("Failed to write compressed file")
+        })
 }
 
-fn compressible_files(entry: &DirEntry) -> bool {
-    let is_file = entry.file_type().is_file();
+fn compressible_files(entry: &PathBuf) -> bool {
+    let is_file = entry.is_file();
     let is_valid_extension = entry
-        .path()
         .extension()
         .is_some_and(|ext| VALID_EXTENSIONS.iter().any(|valid_ext| ext == *valid_ext));
 
